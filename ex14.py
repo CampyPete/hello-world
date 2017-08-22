0f90cb367f7d501ee96bb2495acfc0cf7ff86425
CampyPete <31081728+CampyPete@users.noreply.github.com>
2017-08-22 11:24:28 -0400

    Update ex14.py

--- ex14.py
+++ ex14.py
@@ -5,12 +5,12 @@ from sys import argv
 script, user_name = argv
 prompt = '> '
 
-print "Hi %s, I'm the %s script." %s (user_name, script)
+print "Hi %s, I'm the %s script." %(user_name, script)
 print "I'd like to ask you a few questions."
-print "Do you like me %s?" % user_name
+print "Do you like me %s?" %user_name
 likes = raw_input(prompt)
 
-print "Where do you live %s?" % user_name
+print "Where do you live %s?" %user_name
 lives = raw_input(prompt)
 
 print "What kind of computer do you have?"
@@ -20,4 +20,4 @@ print """
 Alright, so you said %r about liking me.
 You live in %r.  Not sure where that is.
 And you have a %r computer. Nice.
-""" % (likes, lives, computer)
+""" %(likes, lives, computer)
